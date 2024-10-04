41f1f13607a318cb6eb60b07ca01b8da8283f42f
AlekseySapi <altmind92@gmail.com>
2024-10-04 23:46:26 +0300

    Первый коммит: Добавлены первые программы

---
+++ calc.py
@@ -0,0 +1,6 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите первое число: "))
+
+sum = num1 + num2
+
+print(f"Сумма чисел: {sum}")
---
+++ user_info.py
@@ -0,0 +1,7 @@
+name = "Алексей"
+age = 33
+city = "Токио"
+
+print(f"Имя: {name}")
+print(f"Возраст: {age}")
+print(f"Город: {city}")
