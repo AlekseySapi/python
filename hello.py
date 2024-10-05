c21050ed750699e57353d541e39101ebaf870deb
AlekseySapi <altmind92@gmail.com>
2024-10-05 20:42:01 +0300

    Добавил проверку возраста, изучаю if, elif, else

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 print("Привет, мир!")
 
-user_name = input("Как тебя зовут? ")
-print(f"Привет, {user_name}")
+user_name = input("... Постой, а как тебя зовут? ")
+print(f"Привет, {user_name}!")
+
+user_age = int(input("Подскажи, сколько тебе лет: "))
+
+if user_age < 13:
+    print("Ты пока ещё ребёнок")
+elif user_age >= 13 and user_age < 18:
+    print("Ты подросток")
+elif user_age >= 18 and user_age < 65:
+    print("Ты взрослый человек")
+else:
+    print("Ты пожилой")
