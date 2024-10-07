7a5ac23698538ae72a411d57f82001c896f255e2
AlekseySapi <altmind92@gmail.com>
2024-10-07 21:07:14 +0300

    Вариант использования цикла while

--- hello.py
+++ hello.py
@@ -3,7 +3,16 @@ print("Привет, мир!")
 user_name = input("... Постой, а как тебя зовут? ")
 print(f"Привет, {user_name}!")
 
-user_age = int(input("Подскажи, сколько тебе лет: "))
+
+while True:
+    user_input = input("Введите сколько вам лет (или напишите 'stop', чтобы выйти): ")
+
+    if user_input.lower() == 'stop':
+        print("Программа завершена.")
+        break
+
+    user_age = int(user_input)
+
 
 if user_age < 13:
     print("Ты пока ещё ребёнок")
@@ -11,5 +20,7 @@ elif user_age >= 13 and user_age < 18:
     print("Ты подросток")
 elif user_age >= 18 and user_age < 65:
     print("Ты взрослый человек")
-else:
+elif user_age >= 65:
     print("Ты пожилой")
+else:
+    print("?")
