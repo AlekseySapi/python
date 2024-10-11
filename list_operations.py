1ebad622c2c0eeaa64c6fad29cf27f2f1c197e73
AlekseySapi <altmind92@gmail.com>
2024-10-11 09:32:47 +0300

    Потренировался в операциях над списком

--- list_operations.py
+++ list_operations.py
@@ -1,4 +1,26 @@
 # Список с моими любимыми фильмами
 
-favorite_movies = ["Расплата", "Прибытие"]
-print(favorite_movies)
+favorite_movies = ["Расплата", "Прибытие", "Чудо на Гудзоне"]
+print("Исходный список:", favorite_movies)
+
+# Добавление нового фильма
+favorite_movies.append("Хранители")
+print("После добавления:", favorite_movies)
+
+# Вставка фильма на вторую позицию
+favorite_movies.insert(1, "Области тьмы")
+print("После вставки:", favorite_movies)
+
+# Удаление фильма
+favorite_movies.remove("Чудо на Гудзоне")
+print("После удаления:", favorite_movies)
+
+
+# Длина списка
+print("Количество фильмов:", len(favorite_movies))
+
+
+# Перебор и вывод фильмов
+print("Список любимых фильмов:")
+for movie in favorite_movies:
+    print("-", movie)
