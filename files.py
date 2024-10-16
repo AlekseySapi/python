d40a6989b03f5572cfdeecc1a5a4746775863c34
AlekseySapi <altmind92@gmail.com>
2024-10-16 23:45:55 +0300

    Рассмотрел запись данных в файл

--- files.py
+++ files.py
@@ -7,3 +7,12 @@
 with open('example.txt', 'r', encoding='utf-8') as file:
     text = file.read()
     print(text)
+
+# Запись данных в файл
+with open('example.txt', 'w', encoding='utf-8') as file:
+    file.write("Привет, мир!\n")
+    file.write("Это пример записи в файл.")
+
+# Добавление данных в конец файла
+with open('example.txt', 'a', encoding='utf-8') as file:
+    file.write("\nДобавленная строка.")
