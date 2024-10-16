96d6a6a6a0d41636eb92ec371c18738e9f776a13
AlekseySapi <altmind92@gmail.com>
2024-10-17 00:18:16 +0300

    Изучил работу с модулями

---
+++ my_module.py
@@ -0,0 +1,6 @@
+# Мой модуль
+
+def hello(name):
+    return f"Привет, {name}!"
+
+PI = 3.14159265
---
+++ test_my_module.py
@@ -0,0 +1,6 @@
+# Использую здесь данные и функции из своего модуля
+
+import my_module
+
+print(my_module.hello("Алексей"))
+print("Число Пи = " + str(my_module.PI))
