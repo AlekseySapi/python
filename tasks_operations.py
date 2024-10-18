7b9debbf225abaed4758a1e9583af6537f65d73c
AlekseySapi <altmind92@gmail.com>
2024-10-18 23:51:06 +0300

    Задание выполнено!

--- tasks_operations.py
+++ tasks_operations.py
@@ -5,10 +5,31 @@
 # Снова читает и выводит обновлённое содержимое.
 
 
-# Запись задач в файл
-with open('tasks.txt', 'w', encoding='utf-8') as file:
+file_name = 'tasks.txt'
+
+# Создание файла и запись задач в него
+with open(file_name, 'w', encoding='utf-8') as file:
     file.write("1. Изучить Python\n")
     file.write("2. Решить задачи на LeetCode\n")
     file.write("3. Создать проект на GitHub\n")
 
 print("Задачи записаны в файл.")
+
+# Чтение и вывод содержимого файла
+with open(file_name, 'r', encoding='utf-8') as file:
+    text = file.read()
+    print(f"\nСодержимое файла {file_name}:")
+    print(text)
+
+# Добавление новой задачи
+with open('tasks.txt', 'a', encoding='utf-8') as file:
+    file.write("4. Пройти курс по машинному обучению\n")
+
+print("Новая задача добавлена.")
+
+
+# Повторное чтение и вывод обновлённого содержимого файла
+with open(file_name, 'r', encoding='utf-8') as file:
+    text = file.read()
+    print(f"\nОбновлённое содержимое файла {file_name}:")
+    print(text)
