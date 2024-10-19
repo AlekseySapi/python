2f4866d6b8e733676a4483ddff1b1090af75e572
AlekseySapi <altmind92@gmail.com>
2024-10-19 23:32:05 +0300

    Потренировался со своим модулем

--- my_module.py
+++ my_module.py
@@ -4,3 +4,6 @@ def hello(name):
     return f"Привет, {name}!"
 
 PI = 3.14159265
+
+def circle_area(radius):
+    return PI * radius ** 2
--- test_my_module.py
+++ test_my_module.py
@@ -4,3 +4,8 @@ import my_module
 
 print(my_module.hello("Алексей"))
 print("Число Пи = " + str(my_module.PI))
+
+
+r = float(input("=== === ===\nВведите радиус круга: "))
+area = my_module.circle_area(r)
+print(f"Площадь круга с радиусом {r} = {area}")
