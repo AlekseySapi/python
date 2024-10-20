e8c59a54455206f884fa47990962863210b83235
AlekseySapi <altmind92@gmail.com>
2024-10-20 11:33:58 +0300

    Добавил операцию вычисления степени двойки

--- my_module.py
+++ my_module.py
@@ -7,3 +7,6 @@ PI = 3.14159265
 
 def circle_area(radius):
     return PI * radius ** 2
+
+def power_of_two(n):
+    return 2 ** n
--- test_my_module.py
+++ test_my_module.py
@@ -6,6 +6,20 @@ print(my_module.hello("Алексей"))
 print("Число Пи = " + str(my_module.PI))
 
 
+'''
 r = float(input("=== === ===\nВведите радиус круга: "))
 area = my_module.circle_area(r)
 print(f"Площадь круга с радиусом {r} = {area}")
+'''
+
+while True:
+    while True:
+        user_input = input("=== === ===\nВведите степень двойки: ")
+        try:
+            p = int(user_input)  # Пробуем преобразовать в число
+            break  # Выход из цикла, если ввод корректный
+        except ValueError:
+            print("Ошибка: Нужно вводить только числа. Попробуйте снова.")
+
+    res = my_module.power_of_two(p)
+    print(f"2^{p} = {res}")
