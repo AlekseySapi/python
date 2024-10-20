f092d3d58b6a09b6d0180b4ebe3d3c12f02a2b8b
AlekseySapi <altmind92@gmail.com>
2024-10-20 12:03:13 +0300

    Зациклил запросы

--- api_request.py
+++ api_request.py
@@ -3,9 +3,11 @@
 
 import requests
 
-
-response = requests.get('https://api.github.com')
-if response.status_code == 200:
-    print("Успешное соединение с GitHub API!")
-else:
-    print(f"Ошибка: статус код {response.status_code}")
+while True:
+    user_ans = input("=== === ===\nПроверим соединение с GitHub API?  y/n: ")
+    if user_ans == 'y' or user_ans == 'Y':
+        response = requests.get('https://api.github.com')
+        if response.status_code == 200:
+            print(">>> Успешное соединение с GitHub API!")
+        else:
+            print(f"Ошибка: статус код {response.status_code}")
