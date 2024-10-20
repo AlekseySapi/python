a00fe1aedc121db02435d97a17c87a0139edfd38
AlekseySapi <altmind92@gmail.com>
2024-10-20 14:17:39 +0300

    Изучил наследование

--- classes.py
+++ classes.py
@@ -12,3 +12,17 @@ class Human:
 human1 = Human("Алексей", 31)
 human1.hello()
 
+
+# Наследование
+
+class Worker(Human):
+    def __init__(self, name, age, job):
+        super().__init__(name, age)  # Вызываем конструктор родительского класса
+        self.job = job
+
+worker1 = Worker("Виктор", 40, "Инженер")
+
+print("=== ===")
+print(f"Имя работника: {worker1.name}")
+print(f"Возраст: {worker1.age}")
+print(f"Должность: {worker1.job}")
