9ce1c8c2ba79026e69a34c579debf5bd7b271568
AlekseySapi <altmind92@gmail.com>
2024-10-20 14:52:18 +0300

    Изучил инкапсуляцию

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 # Класс - это шаблон для создания объектов
 
 class Human:
-    def __init__(self, name, age): # Задаём начальные значения
+    def __init__(self, name, age):      # Задаём начальные значения
         self.name = name
         self.age = age
 
@@ -17,7 +17,7 @@ human1.hello()
 
 class Worker(Human):
     def __init__(self, name, age, job):
-        super().__init__(name, age)  # Вызываем конструктор родительского класса
+        super().__init__(name, age)     # Вызываем конструктор родительского класса
         self.job = job
 
 worker1 = Worker("Виктор", 40, "Инженер")
@@ -26,3 +26,23 @@ print("=== ===")
 print(f"Имя работника: {worker1.name}")
 print(f"Возраст: {worker1.age}")
 print(f"Должность: {worker1.job}")
+
+
+# Инкапсуляция - скрытие атрибутов и методов из публичного доступа
+
+class Account:
+    def __init__(self):
+        self.__cash = 0     # Приватный атрибут
+        
+    def add_cash(self, money):
+        self.__cash += money
+
+    def get_cash(self):
+        return self.__cash
+    
+my_acc = Account()
+
+print("===")
+print(f"Начальный баланс: {my_acc.get_cash()}")
+my_acc.add_cash(1000)       # Добавляем 1000 кредитов
+print(f"Текущий баланс: {my_acc.get_cash()}")
