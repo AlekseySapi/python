00a569c52b025420f21e5733d84f56849385adad
AlekseySapi <altmind92@gmail.com>
2024-12-09 00:23:16 +0300

    Fix

--- coin_flip.py
+++ coin_flip.py
@@ -3,18 +3,18 @@ import random
 # Подбрасывание монеты n раз
 n = 1000
 
+
 while True:
-    while True:
-        user_input = input("Введите y/Y: ")
-        if(user_input == "y" or user_input == "Y"):
+    user_input = input("Введите y/Y: ")
+    if(user_input == "y" or user_input == "Y"):
 
-            flips = [random.choice(['Орёл', 'Решка']) for _ in range(n)]
+        flips = [random.choice(['Орёл', 'Решка']) for _ in range(n)]
 
-            # Подсчёт частоты
-            heads_count = flips.count('Орёл')
-            tails_count = flips.count('Решка')
+        # Подсчёт частоты
+        heads_count = flips.count('Орёл')
+        tails_count = flips.count('Решка')
 
-            print("\n=== Результаты подбрасывания монеты ===")
-            print(f"Орёл  - {heads_count} раз(а) ({int((heads_count / n) * 100)}%)")
-            print(f"Решка - {tails_count} раз(а) ({int((tails_count / n) * 100)}%)")
-            print("=== === === === === === === === === ===\n")
+        print("\n=== Результаты подбрасывания монеты ===")
+        print(f"Орёл  - {heads_count} раз(а) ({int((heads_count / n) * 100)}%)")
+        print(f"Решка - {tails_count} раз(а) ({int((tails_count / n) * 100)}%)")
+        print("=== === === === === === === === === ===\n")
--- dice_toss.py
+++ dice_toss.py
@@ -3,17 +3,17 @@ import random
 # Подбрасывание кубика n раз
 n = 1000
 
+
 while True:
-    while True:
-        user_input = input("Введите y/Y: ")
-        if(user_input == "y" or user_input == "Y"):
+    user_input = input("Введите y/Y: ")
+    if(user_input == "y" or user_input == "Y"):
 
-            rolls = [random.randint(1, 6) for _ in range(n)]
+        rolls = [random.randint(1, 6) for _ in range(n)]
 
-            # Подсчёт частоты выпадения каждой стороны
-            roll_counts = {i: rolls.count(i) for i in range(1, 7)}
+        # Подсчёт частоты выпадения каждой стороны
+        roll_counts = {i: rolls.count(i) for i in range(1, 7)}
 
-            print("\n=== Результаты подбрасывания кубика ===")
-            for side, count in roll_counts.items():
-                print(f"Грань {side}: {count} раз(а) ({int((count / n) * 100)}%)")
-            print("=== === === === === === === === === ===\n")
+        print("\n=== Результаты подбрасывания кубика ===")
+        for side, count in roll_counts.items():
+            print(f"Грань {side}: {count} раз(а) ({int((count / n) * 100)}%)")
+        print("=== === === === === === === === === ===\n")
