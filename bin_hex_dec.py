64735829c8137df4fe28f7959585e9255474f5e7
AlekseySapi <altmind92@gmail.com>
2024-12-21 07:46:09 +0300

    Обновил оформление, разделил переводы на группы

--- bin_hex_dec.py
+++ bin_hex_dec.py
@@ -12,35 +12,63 @@ def is_bin(num):
     except ValueError:
         return False
 
+line = "===== ===== ===== ===== ===== ===== ====="
+line2 = "===== ===== =====\n  [db / hb] -> 2,\n  [bh / dh] -> 16,\n  [bd / hd] -> 10:"
+
 
 def main():
-     
+    
+    print(line)
+    print("В какую систему счисления перевести число?")
+    print("Введите db, hb, bh, dh, bd или hd:")
+    print("  db: 10 -> 2\n  hb: 16 -> 2\n  bh: 2 -> 16\n  dh: 10 -> 16\n  bd: 2 -> 10\n  hd: 16 -> 10")
+
     while True:
-        print("===== ===== =====")
         while True:
-            choice = input("В какую систему счисления перевести число?\nВведите 2 (b), 16 (h) или 10 (d):\n> ")
-            if choice == "2" or choice == "b" or choice == "16" or choice == "h" or choice == "10" or choice == "d":
+            choice = input("> ")
+            if choice == "db" or choice == "hb" or choice == "bh" or choice == "dh" or choice == "bd" or choice == "hd":
                 break
 
-        if choice == "2" or choice == "b":
+        if choice == "db":
             decimal = int(input("Число: "))
             binary = bin(decimal)[2:]
-            print(f"  {decimal} -> {binary}\n")
-        elif choice == "16" or choice == "h":
+            print(f"  {decimal} (d) -> {binary} (b)\n")
+            print(line2)
+        elif choice == "hb":
+            num = input("Число: ")
+            if is_hex(num):
+                decimal = int(num, 16)
+            binary = bin(decimal)[2:]
+            print(f"  {num} (h) -> {binary} (b)\n")
+            print(line2)
+        elif choice == "bh":
+            num = input("Число: ")
+            if is_bin(num):
+                decimal = int(num, 2)
+            hex_num = hex(decimal)[2:]
+            print(f"  {num} (b) -> {hex_num.upper()} (h)\n")
+            print(line2)
+        elif choice == "dh":
             decimal = int(input("Число: "))
             hex_num = hex(decimal)[2:]
-            print(f"  {decimal} -> {hex_num.upper()}\n")
-        elif choice == "10" or choice == "d":
+            print(f"  {decimal} (d) -> {hex_num.upper()} (h)\n")
+            print(line2)
+        elif choice == "bd":
             num = input("Число: ")
-            
             if is_bin(num):
                 decimal = int(num, 2)  # 2 указывает на основание, если есть префикс - можно указать 0
-            elif is_hex(num):
-                decimal = int(num, 16)
+                print(f"  {num} (b) -> {decimal} (d)\n")
             else:
                 exit()
-            
-            print(f"  {num} -> {decimal}\n")
+            print(line2)
+        elif choice == "hd":
+            num = input("Число: ")
+            if is_hex(num):
+                decimal = int(num, 16)
+                print(f"  {num} (h) -> {decimal} (d)\n")
+            else:
+                exit()
+            print(line2)
 
 
 if __name__ == "__main__":
