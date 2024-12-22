13a2a0c43e53cabb782dc07927c980147342fe90
AlekseySapi <altmind92@gmail.com>
2024-12-23 00:48:50 +0300

    Перевод кодов пробела, табуляции и новой строки в символы

--- chars_codes.py
+++ chars_codes.py
@@ -7,5 +7,16 @@ while True:
     for char in text:
         char_code = ord(char)
         print(f"'{char}' - {char_code}")
+
+    # Перевод кодов пробела, табуляции и новой строки в символы
+    space_char_code = 32    # 0b100000
+    t_char_code = 9         # 0b1001
+    n_char_code = 10        # 0b1010
+
+    print("\n=== === ===")
+    print(f"Пробел - '{chr(space_char_code)}'")
+    print(f"Табуляция - '{chr(t_char_code)}'")
+    print(f"Перенос строки - '{chr(n_char_code)}'")
+
     
     input()
