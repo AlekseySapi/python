2a91de77b71b5e203f894311562bc011fb8b2b50
AlekseySapi <altmind92@gmail.com>
2024-12-25 03:39:53 +0300

    Топ частоты букв в тексте

---
+++ top_letters.py
@@ -0,0 +1,36 @@
+import os
+from collections import Counter
+
+line = '\n======= ======= ======='
+
+
+print(line)
+while True:
+
+    while True:
+        file_path = input("Введите путь к файлу:\n> ").strip()
+        if not os.path.exists(file_path):
+            print("Файл не найден.")
+        else:
+            break
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+
+    # Оставляем только буквы, приводим к верхнему регистру
+    letters = [char.upper() for char in text if char.isalpha()]
+
+    # Подсчитываем частоту каждой буквы
+    letter_counts = Counter(letters)
+
+    # Получаем топ самых частых букв
+    n = 10
+    top = letter_counts.most_common(10)
+
+
+    print(line)
+    print(f"Топ {n} самых частых букв:")
+    for i, (letter, count) in enumerate(top, start=1):
+        print(f"{i}) '{letter}'  x{count}")
+    print(line)
