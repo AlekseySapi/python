855b2f73dc671fca328b6cf7f1bdd74e08d32922
AlekseySapi <altmind92@gmail.com>
2025-01-15 22:18:05 +0300

    Решаю задачи

---
+++ tasks/anagram.py
@@ -0,0 +1,7 @@
+def is_anagram(s1, s2):
+    # TODO: Определить - являются ли слова анаграммами
+    pass
+
+
+print(is_anagram("listen", "silent"))  # True
+print(is_anagram("hello", "world"))   # False
