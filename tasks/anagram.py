0b87b6649c01bf588e2b12b7b484076707c27c63
AlekseySapi <altmind92@gmail.com>
2025-01-15 22:43:46 +0300

    Проверка на анаграммы

--- tasks/anagram.py
+++ tasks/anagram.py
@@ -1,6 +1,9 @@
 def is_anagram(s1, s2):
     # TODO: Определить - являются ли слова анаграммами
-    pass
+    # pass
+    if len(s1) != len(s2):
+        return False
+    return sorted(s1) == sorted(s2)
 
 
 print(is_anagram("listen", "silent"))  # True
