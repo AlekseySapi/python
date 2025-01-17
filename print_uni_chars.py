e0acdd6b4590c818eaeb8f121b9e190457a6af82
AlekseySapi <altmind92@gmail.com>
2025-01-17 07:41:49 +0300

    Печать диапазона символов Unicode

---
+++ print_uni_chars.py
@@ -0,0 +1,23 @@
+import pyperclip
+
+print()
+chars = []
+char = 'ꀀ'  # Первый символ
+char_ord = ord(char)
+n = 1160  # Количество символов
+i = 0
+for c in range(char_ord, char_ord + n):
+    chars.append(chr(c))
+    chars.append(' ')
+    i += 1
+    if (i % 20) == 0:
+        chars.append('\n')
+
+chars_str = ''.join(chars)
+
+print(chars_str)
+
+pyperclip.copy(chars_str)
+print("\n>> Символы скопированы в буфер обмена.\n")
+
+print()
