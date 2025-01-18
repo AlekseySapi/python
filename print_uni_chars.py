ebca6ccb1268525fd5dd307d0d8a48730e3ba6ed
AlekseySapi <altmind92@gmail.com>
2025-01-18 14:54:47 +0300

    Добавил вывод в файл

--- print_uni_chars.py
+++ print_uni_chars.py
@@ -1,24 +1,44 @@
-import pyperclip
+import os
+# import pyperclip
 
-print()
-chars = []
-char = 'ꀀ'  # Первый символ
-char_ord = ord(char)
-n = 1160  # Количество символов
-i = 0
-for c in range(char_ord, char_ord + n):
-    chars.append(chr(c))
-    chars.append(' ')
-    i += 1
-    if (i % 20) == 0:
-        chars.append('\n')
+line = '\n####### ####### #######'
 
-chars_str = ''.join(chars)
 
-print(chars_str)
+def main():
+    while True:
+        print(line)
+        file_path = input("# Введите путь к файлу:\n> ").strip()
+        if not os.path.exists(file_path):
+            print("Файл не найден.")
+        else:
+            break
 
-pyperclip.copy(chars_str)
-print("\n>> Символы скопированы в буфер обмена.\n")
+    chars = []
+    char = 'ꀀ'  # Первый символ
+    char_ord = ord(char)
+    n = 1160  # Количество символов
+    i = 0
+    for c in range(char_ord, char_ord + n):
+        chars.append(chr(c))
+        chars.append(' ')
+        i += 1
+        if (i % 20) == 0:
+            chars.append('\n')
 
-print()
-input()
+    chars_str = ''.join(chars)
+
+
+    with open(file_path, 'a', encoding='utf-8') as file:
+        file.write("\n\n\n========= ======= ======= ======= =========\n")
+        file.write(f"{chars_str}")
+
+    print("Символы записаны в файл.")
+
+    # pyperclip.copy(chars_str)
+    # print("\n>> Символы скопированы в буфер обмена.\n")
+
+    print(line)
+
+
+if __name__ == "__main__":
+    main()
