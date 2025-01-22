6f791a8456304c103fad69529b68c6e39b78d499
AlekseySapi <altmind92@gmail.com>
2025-01-22 20:24:22 +0300

    Задача решена

--- tasks/two_sum.py
+++ tasks/two_sum.py
@@ -1,6 +1,15 @@
 def two_sum(nums, target):
     # TODO: Определить два числа, которые в сумме дают второе
-    pass
+    # pass
+    # Создаем словарь для хранения чисел и их индексов
+    seen = {}
+    for i, num in enumerate(nums):
+        num_2 = target - num
+        # Если дополнение уже есть в словаре, возвращаем индексы
+        if num_2 in seen:
+            return [seen[num_2], i]
+        # Иначе добавляем текущее число в словарь
+        seen[num] = i
 
 # Примеры:
 print(two_sum([2, 7, 11, 15], 9))  # [0, 1]
