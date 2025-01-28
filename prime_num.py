4177e4d461d23fe93409b823b3d26984247f306d
AlekseySapi <altmind92@gmail.com>
2025-01-28 22:15:29 +0300

    Оптимизировал логику

--- prime_num.py
+++ prime_num.py
@@ -4,19 +4,19 @@ line = '\n######## ######## ########'
 
 
 def is_prime(n):
-    if n == 2:
+    if n in (2, 3):
         return True
-    if n % 2 == 0:
+    if n % 2 == 0 or n % 3 == 0:
         return False
     
-    i = 3
+    i = 5
     sqrt = math.isqrt(n)  # Округление корня в меньшую сторону
     # print(f'Корень = {sqrt}')
     while i <= sqrt:
         # print(i)
-        if n % i == 0:
+        if n % i == 0 or n % (i + 2) == 0:  # Рассматриваем по паре чисел в областях, кратных 6+-1
             return False
-        i += 2
+        i += 6
     return True
 
 
