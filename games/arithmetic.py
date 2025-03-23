30e69bd72260463dd8db685f4a9f43ab3219f5e2
AlekseySapi <altmind92@gmail.com>
2025-03-23 23:55:26 +0300

    Обновил логику и дизайн

--- games/arithmetic.py
+++ games/arithmetic.py
@@ -1,6 +1,6 @@
 import random
 
-line = '\n============== ============== =============='
+line = '\n================ ================ ================'
 
 def get_random_number(digits):
     return random.randint(10**(digits-1), 10**digits - 1)
@@ -17,44 +17,54 @@ def get_operation_result(num1, num2, operation):
 
 def main():
     print('\nТренировка арифметики')
-    print('\nВыберите действие: 1 - [+], 2 - [-], 3 - [*], 4 - [/]')
-    operation_choice = ''
-    while operation_choice not in ('1', '2', '3', '4'):
-        operation_choice = input('> ').strip()
-
-    operation_mapping = {'1': '+', '2': '-', '3': '*', '4': '/'}
-    operation = operation_mapping[operation_choice]
-    
-    digits1, digits2 = 1, 1
-    correct_answers = 0
-    
+    print('\n для возврата к выбору действия, введите ->  q\n')
     while True:
-        num1 = get_random_number(digits1)
-        num2 = get_random_number(digits2)
-        if operation in ('*', '/'):
-            while num2 == 1:
-                num2 = get_random_number(digits2)
+        print('\nВыберите действие: 1: [+]  2: [-]  3: [*]  4: [/]')
+        operation_choice = ''
+        s = ''
+        while operation_choice not in ('1', '2', '3', '4'):
+            s += '>'
+            operation_choice = input(f'{s} ').strip()
+
+        operation_mapping = {'1': '+', '2': '-', '3': '*', '4': '/'}
+        operation = operation_mapping[operation_choice]
         
-        correct_result = get_operation_result(num1, num2, operation)
-        print(line)
+        digits1, digits2 = 1, 1
+        correct_answers = 0
+
+        keep_playing = True
         
-        while True:
-            user_answer = input(f'   {num1} {operation} {num2} = ').replace(',', '.')
-            try:
-                if float(user_answer) == correct_result:
-                    print('✅ Верно!')
-                    correct_answers += 1
+        while keep_playing:
+            num1 = get_random_number(digits1)
+            num2 = get_random_number(digits2)
+            if operation in ('*', '/'):
+                while num2 == 1:
+                    num2 = get_random_number(digits2)
+            
+            correct_result = get_operation_result(num1, num2, operation)
+            print(line)
+            
+            while True:
+                user_answer = input(f'   {num1} {operation} {num2} = ').replace(',', '.')
+                if user_answer.lower() == 'q':
+                    keep_playing = False
+                    print(line)
                     break
+                try:
+                    if float(user_answer) == correct_result:
+                        print('✅ Верно!')
+                        correct_answers += 1
+                        break
+                    else:
+                        print('❌ Неверно, попробуйте ещё раз.\n')
+                except ValueError:
+                    print('⚠️  Введите число!\n')
+                    
+            if correct_answers % 3 == 0:
+                if digits1 == digits2:
+                    digits1 += 1
                 else:
-                    print('❌ Неверно, попробуйте ещё раз.\n')
-            except ValueError:
-                print('⚠️  Введите число!\n')
-                
-        if correct_answers % 3 == 0:
-            if digits1 == digits2:
-                digits1 += 1
-            else:
-                digits2 += 1
+                    digits2 += 1
 
 if __name__ == "__main__":
     main()
--- games/arithmetic_ch.py
+++ games/arithmetic_ch.py
@@ -17,26 +17,64 @@ def get_operation_result(num1, num2, operation):
 
 def get_choices(num, operation):
     choices = [num]
+    num_len = len(str(num))
+    k = 1
+    i = 0
+    for _ in range(num_len):
+        i += 1
+        if i % 3 == 0:
+            k *= 10
     while len(choices) < 4:
         if operation == '/':
             v = random.choice([1, 2, 3])
             if v == 1:
-                rand_num = num + (random.randint(1, 999) / 100)
-                rand_num = round(rand_num, 2)
+                if num % 10 >= 2:
+                    rand_num = num - (random.randint(1, 199) / 100)
+                    rand_num = round(rand_num, 2)
+                elif num % 10 >= 1:
+                    rand_num = num - (random.randint(1, 99) / 100)
+                    rand_num = round(rand_num, 2)
+                else:
+                    rand_num = num + (random.randint(1, 199) / 100)
+                    rand_num = round(rand_num, 2)
             elif v == 2:
-                rand_num = num + (random.randint(1, 499) / 100)
-                rand_num = round(rand_num, 2)
+                if num % 10 >= 2:
+                    rand_num = num - (random.randint(1, 199) / 100)
+                    rand_num = round(rand_num, 2)
+                elif num % 10 >= 1:
+                    rand_num = num - (random.randint(1, 99) / 100)
+                    rand_num = round(rand_num, 2)
+                else:
+                    rand_num = num + (random.randint(1, 49) / 100)
+                    rand_num = round(rand_num, 2)
             else:
-                rand_num = num + (random.randint(1, 9) / 100)
-                rand_num = round(rand_num, 2)
+                if num % 10 >= 2:
+                    rand_num = num - (random.randint(1, 199) / 100)
+                    rand_num = round(rand_num, 2)
+                elif num % 10 >= 1:
+                    rand_num = num - (random.randint(1, 99) / 100)
+                    rand_num = round(rand_num, 2)
+                else:
+                    rand_num = num + (random.randint(1, 9) / 100)
+                    rand_num = round(rand_num, 2)
         else:
-            v = random.choice([1, 2, 3])
-            if v == 1:
-                rand_num = num + random.randint(1, 99)
-            elif v == 2:
-                rand_num = num + random.randint(1, 49)
+            if (num % 10 * k) > (9 * k):
+                rand_num = num - random.randint(1, 9 * k)
             else:
-                rand_num = num + random.randint(1, 2)
+                rand_num = num + random.randint(1, 9 * k)
+            while rand_num % 10 != num % 10:
+                v = random.choice([1, 2, 3])
+                if v == 1:
+                    rand_num = num + random.randint(1, 49 * k)
+                elif v == 2:
+                    rand_num = num + random.randint(1, 9 * k)
+                else:
+                    if (num % 100 * k) > (19 * k):
+                        rand_num = num - random.randint(1, 9 * k)
+                    elif (num % 10 * k) > (9 * k):
+                        rand_num = num - random.randint(1, 5 * k)
+                    else:
+                        rand_num = num + random.randint(1, 9 * k)
         if rand_num not in choices:
             choices.append(rand_num)
     random.shuffle(choices)
@@ -45,51 +83,64 @@ def get_choices(num, operation):
 
 def main():
     print('\nТренировка арифметики')
-    print('\nВыберите действие: 1: [+]  2: [-]  3: [*]  4: [/]')
-    operation_choice = ''
-    while operation_choice not in ('1', '2', '3', '4'):
-        operation_choice = input('> ').strip()
-
-    operation_mapping = {'1': '+', '2': '-', '3': '*', '4': '/'}
-    operation = operation_mapping[operation_choice]
-    
-    digits1, digits2 = 1, 1
-    correct_answers = 0
-    
+    print('\n для возврата к выбору действия, введите ->  q\n')
     while True:
-        num1 = get_random_number(digits1)
-        num2 = get_random_number(digits2)
-        if operation in ('*', '/'):
-            while num1 == 1:
-                num1 = get_random_number(digits2)
-            while num2 == 1:
-                num2 = get_random_number(digits2)
-        
-        correct_result = get_operation_result(num1, num2, operation)
-        print(line)
-        print(f'   {num1} {operation} {num2} =')
-        choices = get_choices(correct_result, operation)
-        print(f'\n  1: {choices[0]}   2: {choices[1]}   3: {choices[2]}   4: {choices[3]}')
-        
-        while True:
-            user_answer = ''
-            while user_answer not in ('1', '2', '3', '4'):
-                user_answer = input('> ').strip()
+        print('\nВыберите действие: 1: [+]  2: [-]  3: [*]  4: [/]')
+        operation_choice = ''
+        s = ''
+        while operation_choice not in ('1', '2', '3', '4'):
+            s += '>'
+            operation_choice = input(f'{s} ').strip()
 
-            choices_mapping = {'1': choices[0], '2': choices[1], '3': choices[2], '4': choices[3]}
-            choice = choices_mapping[user_answer]
-            if choice == correct_result:
-                print(' ✅ Верно!')
-                correct_answers += 1
-                break
-            else:
-                print(' ❌ Неверно, попробуйте ещё раз.\n')
+        operation_mapping = {'1': '+', '2': '-', '3': '*', '4': '/'}
+        operation = operation_mapping[operation_choice]
+        
+        digits1, digits2 = 1, 1
+        correct_answers = 0
+
+        keep_playing = True
+        
+        while keep_playing:
+            num1 = get_random_number(digits1)
+            num2 = get_random_number(digits2)
+            if operation in ('*', '/'):
+                while num1 == 1:
+                    num1 = get_random_number(digits2)
+                while num2 == 1:
+                    num2 = get_random_number(digits2)
+            
+            correct_result = get_operation_result(num1, num2, operation)
+            print(line)
+            print(f'   {num1} {operation} {num2} =')
+            choices = get_choices(correct_result, operation)
+            print(f'\n  1: {choices[0]}   2: {choices[1]}   3: {choices[2]}   4: {choices[3]}')
+            
+            while True:
+                user_answer = ''
+                s = ''
+                while user_answer.lower() not in ('1', '2', '3', '4', 'q'):
+                    s += '>'
+                    user_answer = input(f'{s} ').strip()
                 
-        if correct_answers % 3 == 0:
-            if digits1 == digits2:
-                digits1 += 1
-            else:
-                digits2 += 1
+                if user_answer.lower() == 'q':
+                    keep_playing = False
+                    print(line)
+                    break
+
+                choices_mapping = {'1': choices[0], '2': choices[1], '3': choices[2], '4': choices[3]}
+                choice = choices_mapping[user_answer]
+                if choice == correct_result:
+                    print(' ✅ Верно!')
+                    correct_answers += 1
+                    break
+                else:
+                    print(' ❌ Неверно, попробуйте ещё раз.\n')
+                    
+            if correct_answers % 3 == 0:
+                if digits1 == digits2:
+                    digits1 += 1
+                else:
+                    digits2 += 1
 
 if __name__ == "__main__":
     main()
