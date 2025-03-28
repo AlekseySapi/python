34e81243c2614fa465a34fff947ff4ceda99356a
AlekseySapi <altmind92@gmail.com>
2025-03-28 22:20:17 +0300

    Добавил ещё одно число (три действия) + задана частота появления действий

--- games/arithm_master.py
+++ games/arithm_master.py
@@ -3,36 +3,38 @@ import random
 line = '\n================ ================ ================'
 
 def get_random_number(digits):
-    ch = random.choice([1, 2])
+    ch = random.choices([1, 2], [7, 3], k=1)[0]	# 70% - вероятность выбора 1
     if ch == 1:
         return random.randint(2, 10**digits - 1)
     else:
         return random.randint(2, 9)
 
 def generate_expression(digits):
-    operations = ['+', '-', '*']
-    op1, op2 = random.choices(operations, k=2)
-    
-    num1, num2, num3 = get_random_number(digits), get_random_number(digits), get_random_number(digits)
-    
-    expr = f"{num1} {op1} {num2} {op2} {num3}"
-    correct_result = eval(expr)
-    
+    operations = ['+', '-', '*', '/']
+    w = [55, 25, 15, 5]
+    op1, op2, op3 = random.choices(operations, weights=w, k=3)
+
+    num1, num2, num3, num4 = get_random_number(digits), get_random_number(digits), get_random_number(digits), get_random_number(digits)
+
+    expr = f"{num1} {op1} {num2} {op2} {num3} {op3} {num4}"
+    correct_result = round(eval(expr), 2)
+
     return expr, correct_result
 
 def main():
     print('\nТренировка арифметики')
-    
+    print('\n(при делении  ->  округляем до сотых)')
+
     digits = 1
     correct_answers = 0
-    
+
     while True:
         expr, correct_result = generate_expression(digits)
         print(line)
-        
+
         while True:
-            user_answer = input(f'   {expr} = ')
-            
+            user_answer = input(f'   {expr} = ').replace(',', '.')
+
             try:
                 if float(user_answer) == correct_result:
                     print('✅ Верно!')
@@ -40,10 +42,11 @@ def main():
                     break
                 else:
                     print('❌ Неверно, попробуйте ещё раз.\n')
+                    correct_answers -= 1
             except ValueError:
                 print('⚠️  Введите число!\n')
-                
-        if correct_answers % 3 == 0:
+
+        if (correct_answers > 0) and (correct_answers % 3 == 0):
             digits += 1
             print(f'\n🔥 Предел генерации чисел повышен до {10**digits - 1}!')
 
