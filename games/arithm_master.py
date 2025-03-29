050d443959045575cf935c80bba20bf4d2e487ab
AlekseySapi <altmind92@gmail.com>
2025-03-29 14:31:36 +0300

    Добавил функции Начать заново, Узнать ответ и поправил подсчёт верных ответов

--- games/arithm_master.py
+++ games/arithm_master.py
@@ -9,9 +9,13 @@ def get_random_number(digits):
     else:
         return random.randint(2, 9)
 
-def generate_expression(digits):
-    operations = ['+', '-', '*', '/']
-    w = [55, 25, 15, 5]
+def generate_expression(digits, op_choice):
+    if op_choice == '1':
+        operations = ['+', '-', '*', '/']
+        w = [55, 25, 15, 5]
+    else:
+        operations = ['+', '-', '*']
+        w = [55, 25, 20]
     op1, op2, op3 = random.choices(operations, weights=w, k=3)
 
     num1, num2, num3, num4 = get_random_number(digits), get_random_number(digits), get_random_number(digits), get_random_number(digits)
@@ -24,31 +28,51 @@ def generate_expression(digits):
 def main():
     print('\nТренировка арифметики')
     print('\n(при делении  ->  округляем до сотых)')
-
-    digits = 1
-    correct_answers = 0
-
+    print('\n\n Чтобы начать заново, введите ->  q\n')
+    print(' Для получения ответа, введите ->  pass\n')
     while True:
-        expr, correct_result = generate_expression(digits)
         print(line)
+        print('  1 - Добавить операции деления, 2 - Без них')
+        op_choice = ''
+        s = ''
+        while op_choice not in ('1', '2'):
+            s += '>'
+            op_choice = input(f'{s} ').strip()
+        digits = 1
+        correct_answers = 0
+        keep_playing = True
+        while keep_playing:
+            expr, correct_result = generate_expression(digits, op_choice)
+            print(line)
 
-        while True:
-            user_answer = input(f'   {expr} = ').replace(',', '.')
-
-            try:
-                if float(user_answer) == correct_result:
-                    print('✅ Верно!')
-                    correct_answers += 1
+            while True:
+                user_answer = input(f'   {expr} = ').replace(',', '.')
+                if user_answer.lower() == 'q':
+                    keep_playing = False
+                    print()
                     break
-                else:
-                    print('❌ Неверно, попробуйте ещё раз.\n')
+                if user_answer.lower() == 'pass':
+                    print(f'\n Ответ: {correct_result}\n')
                     correct_answers -= 1
-            except ValueError:
-                print('⚠️  Введите число!\n')
+                    break
 
-        if (correct_answers > 0) and (correct_answers % 3 == 0):
-            digits += 1
-            print(f'\n🔥 Предел генерации чисел повышен до {10**digits - 1}!')
+                try:
+                    if float(user_answer) == correct_result:
+                        print('✅ Верно!')
+                        correct_answers += 1
+                        # print(f'\n correct_answers = {correct_answers}\n')
+                        break
+                    else:
+                        print('❌ Неверно, попробуйте ещё раз.\n')
+                        correct_answers -= 1
+                        # print(f'( correct_answers = {correct_answers} )\n')
+                except ValueError:
+                    print('⚠️  Введите число!\n')
+
+            if (correct_answers > 0) and (correct_answers % 3 == 0):
+                digits += 1
+                correct_answers = 0
+                print(f'\n🔥 Предел генерации чисел повышен до {10**digits - 1}!')
 
 if __name__ == "__main__":
     main()
