ff925d7fe202f42c5d02fa4ad9003f5f80d2beee
AlekseySapi <altmind92@gmail.com>
2025-04-13 21:29:06 +0300

    Начал реализацию логики

--- games/zenless_gacha.py
+++ games/zenless_gacha.py
@@ -1,2 +1,22 @@
 # Начну реализацию симулятора гачи Zenless Zone Zero
 
+line = '\n################# ################# #################'
+
+
+def main():
+	print('\n        === Симулятор гачи Zenless Zone Zero ===')
+	pools = 100     # Всего круток в наличии
+	while True:
+		print(line)
+		print(f"\n> Всего круток в наличии: {pools}")
+		print("\nСколько крутим? (Введите 1 или 10)")
+		choice = '0'
+		s = ''
+		while choice not in ('1', '10'):
+			s += '>'
+			choice = input(f'{s} ').strip()
+		pools -= int(choice)
+		print(f"\nОсталось {pools} круток")
+
+if __name__ == "__main__":
+	main()
