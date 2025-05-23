bf8c8bc3b9e8d9762bde483e9a48fd173f2b2ea6
AlekseySapi <altmind92@gmail.com>
2025-05-23 23:00:31 +0300

    Доработал логику

--- games/zenless_gacha.py
+++ games/zenless_gacha.py
@@ -4,22 +4,26 @@ import random
 
 line = '\n################# ################# #################'
 
-def print_res(user_pools, pools, choice):
+def check_win(pools, choice):
 	v = ['S', 'poop']
 	if pools < 74:
 		w = [12, 988]
 		for _ in range(int(choice)):
-			res = random.choices(v, weights=w, k=2)
-			if res == 'S':
-				return print("\n  Победа! Вы получили персонажа S-класса!\n")
-			return print(f'\n  К сожалению, пока только "poop"...\n\nОсталось {user_pools} круток')
+			pools += 1
+			if pools == 90:
+				return True, pools
+			res = random.choices(v, weights=w, k=1)
+			if res[0] == 'S':
+				return True, pools
+		return False, pools
 	else:
 		w = [25, 75]
 		for _ in range(int(choice)):
-			res = random.choices(v, weights=w, k=2)
-			if res == 'S':
-				return print("\n  Победа! Вы получили персонажа S-класса!\n")
-			return print(f'\n  К сожалению, пока только "poop"...\n\nОсталось {user_pools} круток')
+			pools += 1
+			res = random.choices(v, weights=w, k=1)
+			if res[0] == 'S':
+				return True, pools
+		return False, pools
 
 
 def main():
@@ -28,7 +32,7 @@ def main():
 	pools = 0		# Откручено
 	while True:
 		print(line)
-		print(f"\n> Всего круток в наличии: {user_pools}")
+		print(f"\n> Всего круток в наличии: {user_pools}\n> Откручено: {pools}")
 		print("\nСколько крутим? (Введите 1 или 10)")
 		choice = '0'
 		s = ''
@@ -37,14 +41,13 @@ def main():
 			choice = input(f'{s} ').strip()
 			if user_pools - int(choice) < 0: choice = '0'	# Вариант обхода недостаточности круток (последняя десятка)
 		user_pools -= int(choice)
-		pools += int(choice)
-		if pools == 90:
-			print("\n  Победа! Вы получили персонажа S-класса!")
-			print(f"")
+
+		win, pools = check_win(pools, choice)
+		if win:
+			print(f"\n  Победа! Вы получили персонажа S-класса! (на {pools} крутке)\n")
 			break
 		else:
-			print_res(user_pools, pools, choice)
-			print(f"")
+			print(f'\n  К сожалению, пока только "poop"...\n\nОсталось {user_pools} круток')
 
 
 if __name__ == "__main__":
