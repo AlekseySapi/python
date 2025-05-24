d18d14895ad151e3f035bd8cb1f3af9b41014699
AlekseySapi <altmind92@gmail.com>
2025-05-24 11:50:49 +0300

    Симулятор гачи ZZZ для версии 1.7 готов!

--- games/zenless_gacha.py
+++ games/zenless_gacha.py
@@ -1,10 +1,10 @@
-# Начну реализацию симулятора гачи Zenless Zone Zero
+# Симулятор гачи Zenless Zone Zero для версии 1.7
 
 import random
 
 line = '\n################# ################# #################'
 
-def check_win(pools, choice):
+def check_win(pools, choice, character):
 	v = ['S', 'poop']
 	if pools < 74:
 		w = [12, 988]
@@ -14,22 +14,29 @@ def check_win(pools, choice):
 				return True, pools
 			res = random.choices(v, weights=w, k=1)
 			if res[0] == 'S':
-				return True, pools
-		return False, pools
+				char_choice = ['Hugo', 'Nekomata', 'Koleda', 'Grace', 'Rina', 'Lycaon', 'Soldier 11']
+				chances = [52, 8, 8, 8, 8, 8, 8]
+				character = random.choices(char_choice, weights=chances, k=1)[0]
+				return True, pools, character
+		return False, pools, character
 	else:
 		w = [25, 75]
 		for _ in range(int(choice)):
 			pools += 1
 			res = random.choices(v, weights=w, k=1)
 			if res[0] == 'S':
-				return True, pools
-		return False, pools
+				char_choice = ['Hugo', 'Nekomata', 'Koleda', 'Grace', 'Rina', 'Lycaon', 'Soldier 11']
+				chances = [52, 8, 8, 8, 8, 8, 8]
+				character = random.choices(char_choice, weights=chances, k=1)[0]
+				return True, pools, character
+		return False, pools, character
 
 
 def main():
 	print('\n        === Симулятор гачи Zenless Zone Zero ===')
 	user_pools = 90     # Всего круток в наличии
 	pools = 0		# Откручено
+	character = ''
 	while True:
 		print(line)
 		print(f"\n> Всего круток в наличии: {user_pools}\n> Откручено: {pools}")
@@ -42,9 +49,9 @@ def main():
 			if user_pools - int(choice) < 0: choice = '0'	# Вариант обхода недостаточности круток (последняя десятка)
 		user_pools -= int(choice)
 
-		win, pools = check_win(pools, choice)
+		win, pools, character = check_win(pools, choice, character)
 		if win:
-			print(f"\n  Победа! Вы получили персонажа S-класса! (на {pools} крутке)\n")
+			print(f"\n  Поздравляем!!! Вы получили персонажа  << {character} >>    (на {pools} крутке)\n")
 			break
 		else:
 			print(f'\n  К сожалению, пока только "poop"...\n\nОсталось {user_pools} круток')
