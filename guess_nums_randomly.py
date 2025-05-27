00ff9e7b7156dc920f49bba8661bc57f97252b25
AlekseySapi <altmind92@gmail.com>
2025-05-27 20:55:34 +0300

    Добавил ввод числа

--- guess_nums_randomly.py
+++ guess_nums_randomly.py
@@ -1,1 +1,28 @@
 # Программа рандомного подбора числовой последовательности
+
+import random
+
+line = '\n################# ################# #################'
+
+
+def main():
+    print('\n     === Подбор числовой последовательности ===')
+    while True:
+        print(line)
+        print("\nВведите число:")
+        s = '>'
+        notNum = True
+        while notNum:
+            try:
+                user_num = int(input(f'{s} ').strip())
+                notNum = False
+            except:
+                s += '>'
+        if user_num < 0: user_num *= -1
+
+
+        input()
+
+
+if __name__ == "__main__":
+	main()
