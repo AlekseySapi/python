c18cfdee0279815536a910b31f2599092e070714
AlekseySapi <altmind92@gmail.com>
2025-06-01 21:36:03 +0300

    Обновил логику и дизайн

--- games/random_calc.py
+++ games/random_calc.py
@@ -24,17 +24,17 @@ def main():
         print()
 
         op = random.randint(0, 2)
-        num = random.randint(1, user_num * 2)
+        n = user_num + random.randint(0, 999)
+        if n < 0: n *= -1
+        num = random.randint(1, n)
         if op == 0:
             res = user_num + num
-            print(f" {user_num} + {num} =  {res}")
         elif op == 1:
             res = user_num - num
-            print(f" {user_num} - {num} =  {res}")
         elif op == 2:
             res = user_num * num
-            print(f" {user_num} * {num} =  {res}")
-        print()
+        if res < 0: res *= -1
+        print(f" >>  {res}  <<\n")
 
         input()
 
