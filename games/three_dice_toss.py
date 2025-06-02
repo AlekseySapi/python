b337389588ee9f4a68f5e2e9e9da4f6baaf7136d
AlekseySapi <altmind92@gmail.com>
2025-06-02 04:04:42 +0300

    Реализовал дизайн и логику до броска кубиков

--- games/three_dice_toss.py
+++ games/three_dice_toss.py
@@ -1,2 +1,29 @@
 # Подбрасывание трёх кубиков
 
+import random
+import time
+
+line = '\n############### ############### ###############'
+
+
+def main():
+    print('\n      === Подбрасывание трёх кубиков ===')
+    while True:
+        random.seed(time.time())
+        print(line)
+        print("\nБросаем кубики? (y/Y):")
+        s = ''
+        answer = ''
+        while True:
+            if answer not in ['y', 'Y']:
+                s += '>'
+                answer = input(f'{s} ').strip()
+            else:
+                break
+        print()
+
+
+
+
+if __name__ == "__main__":
+	main()
