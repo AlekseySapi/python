84841f4fb333869a7afc96eaa48ca669b9fdfbe5
AlekseySapi <altmind92@gmail.com>
2025-06-12 00:31:15 +0300

    Добавил лист с вопросами и переменную ответов

--- games/quiz.py
+++ games/quiz.py
@@ -1,41 +1,43 @@
-# Начну реализовывать программу-опрос
+# Программа-опрос  Sapi Quiz
 
 import random
 
 line = '\n############### ############### ###############'
+q = ["2 * 2 =", "Утро для тебя - это.."]
 
 
 def main():
-    print('\n   === Опрос от Sapi ===')
+    print('\n   === Sapi Quiz ===')
     while True:
+        answers = ''
         print(line)
-        print("\n>> 2 * 2 =\n")
+        print(f"\n>> {q[0]}\n")
         print('1 - "4", 2 - "5", 3 - не знаю, 4 - неважно')
         s = '>'
         while True:
             n1 = input(f"{s} ")
             if n1 in ['1', '2', '3', '4']:
-                n1 = int(n1)
+                answers += n1
                 break
             else:
                 s += '>'
                 continue
         print()
 
-        print("\n>> Утро для тебя - это..\n")
+        print(f"\n>> {q[1]}\n")
         print('1 - Кофе, 2 - Суета, 3 - Медитация, 4 - другое')
         s = '>'
         while True:
             n2 = input(f"{s} ")
             if n2 in ['1', '2', '3', '4']:
-                n2 = int(n2)
+                answers += n2
                 break
             else:
                 s += '>'
                 continue
         print()
 
-        if n1 == 1 and n2 == 1:
+        if answers == '11':
             print("\nКлассно! Вы прошли опрос, ответив на все опросы как и я!\n Добро пожаловать в мою группу:")
             print(" >>  https://t.me/sapi_group  <<")
         else:
