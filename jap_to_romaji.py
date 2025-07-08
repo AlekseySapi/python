6a551749f34c88fbaecb52a8e84c0b2ae6fd1baf
AlekseySapi <altmind92@gmail.com>
2025-07-08 07:04:12 +0300

    Начал реализацию

--- jap_to_romaji.py
+++ jap_to_romaji.py
@@ -1,1 +1,24 @@
 # Конвертация Кандзи, Хираганы и Катаканы в Ромадзи
+
+line = '\n################# ################# #################'
+
+
+def main():
+    print('\n    === Топ символов японской каны по частоте ===')
+    while True:
+        print(line)
+        file_path = 'w.txt'
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        romaji_text = jap_to_romaji(text)
+
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write("\n\n\n=== Ромадзи ===\n")
+            file.write(romaji_text)
+
+        print("\n\n\n  ✅ Конвертация в ромадзи завершена.\n")
+        input()
+
+
+if __name__ == "__main__":
+	main()
--- romaji_to_katakana.py
+++ romaji_to_katakana.py
@@ -1,1 +1,24 @@
 # Конвертация Ромадзи в Катакану
+
+line = '\n################# ################# #################'
+
+
+def main():
+    print('\n    === Топ символов японской каны по частоте ===')
+    while True:
+        print(line)
+        file_path = 'w.txt'
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        katakana_text = romaji_to_katakana(text)
+
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write("\n\n\n=== Катакана ===\n")
+            file.write(katakana_text)
+
+        print("\n\n\n  ✅ Конвертация в катакану завершена.\n")
+        input()
+
+
+if __name__ == "__main__":
+	main()
