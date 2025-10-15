6b750c775d79e648d1750b406d768d0377d73330
AlekseySapi <altmind92@gmail.com>
2025-10-15 20:54:04 +0300

    Классическая расшифровка шифра Виженера

---
+++ ci/ci_vigenere.py
@@ -0,0 +1,58 @@
+line = '\n########## ########## ##########'
+
+abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+zyx = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+
+def vigenere(text, key, alph):
+	result = []
+	key_index = 0
+	for char in text:
+		if char in alph:
+			char_index = alph.index(char)
+			key_char = key[key_index % len(key)]
+			key_shift = alph.index(key_char)
+			new_index = (char_index - key_shift) % len(alph)	# Для шифрования, минус заменить на плюс
+			result.append(alph[new_index])
+			key_index += 1
+		else:
+			result.append(char)
+			continue
+	return ''.join(result)
+
+def clear_key(key):
+	res = []
+	for ch in key:
+		if ch in abc:
+			res.append(ch)
+	return ''.join(res)
+
+
+def main():
+	while True:
+		print('\n  ===== Шифр Виженера =====')
+		while True:
+			print(line)
+
+			print('  1 - Обычный алфавит (ABC..),\n  2 - Перевёрнутый (ZYX..):')
+			choice = ''
+			s = ''
+			while choice not in ('1', '2'):
+				s += '>'
+				choice = input(f'{s} ').strip()
+			if choice == '1':
+				alph = abc
+			else:
+				alph = zyx
+
+			text = input("\n Введите шифр:\n> ").upper()
+			key = input("\n Введите ключ:\n> ").upper()
+			key = clear_key(key)
+
+			print("\n\n Расшифрованный текст:")
+			print(f"> {vigenere(text, key, alph)}")
+			input()
+
+
+
+if __name__ == "__main__":
+    main()
